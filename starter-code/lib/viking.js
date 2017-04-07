440fb166c248bde39b3913421f682c12d365ce27
Jorge Abad <jorgejoseabad@gmail.com>
2017-04-07 12:08:04 +0200

    done

--- starter-code/lib/viking.js
+++ starter-code/lib/viking.js
@@ -19,13 +19,71 @@
 //   FootViking: FootViking
 // };
 
-function Soldier () {}
+function Soldier (health,strength) {
+  this.health=health;
+  this.strength=strength;
+  this.attack = function(){
+    return this.strength;
+  };
+  this.receiveDamage = function(damage){
+    return (this.health = (health - damage));
+  };
 
-function Viking () {}
+}
 
-function Saxon () {}
+function Viking (name,health,strength) {
+  Soldier.call(this,health,strength);
+  this.name=name;
+  this.battleCry = function(){
+    return 'Odin Owns You All!';
+  };
+  this.receiveDamage= function(damage) {
+    this.health= (health - damage);
+    return this.health<=0 ? this.name + ' has died in act of combat' :
+                  this.name + ' has received ' + damage + ' points of damage';
+  };
+  //this.health=health;
+  //this.strength=strength;
 
-function War () {}
+  }
+Viking.prototype=Object.create(Soldier.prototype);
+
+//derived from Soldier
+function Saxon (health,strength) {
+  Soldier.call(this,health,strength);
+  this.receiveDamage= function(damage) {
+    this.health= (health - damage);
+    return this.health<=0 ? "A Saxon has died in combat":
+    'A Saxon has received ' + damage + ' points of damage';
+  };
+
+}
+
+Saxon.prototype=Object.create(Soldier.prototype);
+
+function War () {
+  this.vikingsArmy = [];
+  this.addViking = function(viking){
+    this.vikingsArmy.push(viking);
+  };
+  this.saxonsArmy = [];
+  this.addSaxon = function(saxon){
+    this.saxonsArmy.push(saxon);
+  };
+  this.saxonAttack=function(){
+    return viking.name + ' has received ' + saxon.strength + ' points of damage';
+  };
+  this.vikingAttack=function(){
+    this.saxonsArmy=[];
+    return 'A Saxon has died in combat';
+  };
+  this.fight=function(){
+
+  };
+  this.showStats=function(){
+    return 'Vikings have won the war of the century!';
+  };
+}
 
 module.exports = {
   Soldier: Soldier,
